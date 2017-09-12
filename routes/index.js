3d39290f25d5643d4d8b01013c25309f7b52b2f3
Jeppe Stærk <jeppe@staerk.io>
2017-09-12 15:38:47 +0200

    Update

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Hello World!' });
+  res.render('index', { title: 'E17 - ITTWEB-01 Webudvikling - Gruppe 7 - Mandatory assignment 1' });
 });
 
 module.exports = router;
